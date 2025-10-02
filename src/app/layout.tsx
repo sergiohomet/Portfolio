1b8eff8d992d9cf85940fe5c65514260ac5cb6fb
Sergio Homet <sergiohomet@gmail.com>
2025-10-02 13:31:09 -0300

    Cambios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Portfolio de Sergio Homet',
+  title: 'Sergio Homet',
   description: 'Desarrollador Front End especializado en React y TypeScript.',
 };
 
